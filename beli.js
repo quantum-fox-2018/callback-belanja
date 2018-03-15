7ff2ede7665ef045d96373614f42ecc8b9b47c7c
JoviAndrew <jovian_andrew@hotmail.com>
2018-03-15 15:04:55 +0700

    submit callback belanja

--- beli.js
+++ beli.js
@@ -1,13 +1,14 @@
 function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
+  console.log(`Saya pergi membeli ${obj.name}`)
   setTimeout(function(){
-    let kembalian = uang - obj.harga
-    if (kembalian > 0) {
-      console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
+    let kembalian = uang
+    if (uang - obj.harga > 0) {
+      kembalian = uang - obj.harga
+      console.log(`Saya sudah membeli ${obj.name} uang kembaliannya ${kembalian}`);
       cb(kembalian)
     }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
+      console.log(`uang gk cukup nih buat beli ${obj.name} kembaliannya cuma ${kembalian}`);
+      cb(kembalian)
     }
   }, obj.waktu);
 }
