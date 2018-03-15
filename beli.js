fd588df0bec3ad73f4283b3efb034cc20feb8d26
yasirmaulana <yasir.maulana@gmail.com>
2018-03-15 18:32:43 +0700

    week 2, day 4, callback belanja

--- beli.js
+++ beli.js
@@ -12,4 +12,13 @@ function beli(uang, obj, cb){
   }, obj.waktu);
 }
 
+beli(10000,{item:'permen', harga:2000, waktu:500},function(kembalianYgDidapat){
+  console.log(`men gw beli sabun kembalianya ${kembalianYgDidapat}`)
+})
+
+
+beli(10000,{item:'sabun', harga:9000, waktu:500},function(kembalianLagi){
+  console.log(`terus, gw beli sabun kembaliannya cuma ${kembalianLagi}`)
+})
+
 module.exports = beli;
